c43696afee39469927eacc8814d74b9915578cc8
Jesus Mamani <Jesusm.code@gmail.com>
2020-05-30 09:07:50 -0500

    suiendo empaquetado de agenda app

---
+++ precache-manifest.b0696b9d16b5f0ad66d12863ae057c69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48844a2242bcf1b81a1de4ef5454f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af858a5165bd28fafd1",
+    "url": "/static/js/2.1a4cdf84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a4cdf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c49d27529063bfde2b",
+    "url": "/static/js/main.7cc47e22.chunk.js"
+  },
+  {
+    "revision": "e275a57e96d358f64ebf",
+    "url": "/static/js/runtime-main.6dfc73c8.js"
+  }
+]);
